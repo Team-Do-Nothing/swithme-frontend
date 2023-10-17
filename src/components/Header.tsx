c96b8cd10aad84999419e01af12bb65a2787743c
choify <samuelys@naver.com>
2023-10-17 11:51:55 +0900

    [feat] 레이아웃 반응형 변경 및 헤더 링크 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,21 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex justify-center items-center w-full h-20 overflow-hidden">
+    <div className="fixed flex justify-center items-center w-full h-20 overflow-hidden">
       <div className="flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 w-full max-w-7xl overflow-hidden">
         <div className="w-fit flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 relative overflow-hidden px-5 bg-primary-100 text-4xl text-white">
           Study With Me
         </div>
         <div className="w-full flex justify-start items-center self-stretch flex-grow overflow-hidden">
           <div className="flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 relative overflow-hidden px-10 text-2xl font-medium text-text-100">
-            내 스터디
+            <Link href="/groups">
+              내 스터디
+            </Link>
           </div>
           <div className="flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 relative overflow-hidden px-10 text-2xl font-medium text-text-100">
-            스터디 검색
+            <Link href="/groups">
+              스터디 검색
+            </Link>
           </div>
         </div>
         <div className="w-fit flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-2.5">
