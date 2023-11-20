163197820629cac8e157a199cf7511014016ec81
UBiiiii <willowman55@gmail.com>
2023-11-20 17:32:04 +0900

    [fix] 에러 수정

--- src/components/ModalProvider.tsx
+++ src/components/ModalProvider.tsx
@@ -7,7 +7,11 @@ const ModalProvider = () => {
   const modal = useAppSelector((state) => state.modal.current);
 
   return (
-    <div className="fixed left-0 top-0 w-screen h-screen z-50">
+    <div
+      className={`fixed left-0 top-0 w-screen h-screen z-50 ${
+        modal === "none" && "hidden"
+      }`}
+    >
       {modal === "inviteMember" && <InviteMemberModal />}
     </div>
   );
